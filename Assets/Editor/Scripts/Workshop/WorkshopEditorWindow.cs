3a491bbba56a8b4ea7ff3621bf6b1382ebdc95bb
River Buizel <4911928+rocket0634@users.noreply.github.com>
2023-07-22 08:25:45 -0700

    Update Modkit

--- Assets/Editor/Scripts/Workshop/WorkshopEditorWindow.cs
+++ Assets/Editor/Scripts/Workshop/WorkshopEditorWindow.cs
@@ -1,8 +1,11 @@
-﻿using UnityEditor;
+﻿#pragma warning disable 436
+
+using UnityEditor;
 using Steamworks;
 using UnityEngine;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Collections.Generic;
 
 public class WorkshopEditorWindow : EditorWindow
@@ -34,7 +37,7 @@ public class WorkshopEditorWindow : EditorWindow
         Debug.LogWarning(pchDebugText);
     }
 
-    [MenuItem("Keep Talking ModKit/Steam Workshop Tool _#F6", priority = 20)]
+    [MenuItem("Keep Talking ModKit/Steam Workshop Tool _#F5", priority = 20)]
     protected static void ShowWindow()
     {
         WorkshopEditorWindow window = EditorWindow.GetWindow<WorkshopEditorWindow>("Workshop");
@@ -96,13 +99,15 @@ public class WorkshopEditorWindow : EditorWindow
             GUI.backgroundColor = new Color(0.1f, 0.1f, 0.5f, 0.7f);
             EditorGUILayout.BeginVertical(EditorStyles.helpBox);
             GUI.backgroundColor = oldBGColor;
+            
+            string folder = GetContentPath();
 
             EditorGUILayout.LabelField("Publishing Tools", EditorStyles.largeLabel);
             EditorGUILayout.Separator();
             EditorGUILayout.LabelField("User:", userName);
-            EditorGUILayout.LabelField("Content Folder:", GetContentPath());
+            EditorGUILayout.LabelField("Content Folder:", folder);
 
-            DirectoryInfo dir = new DirectoryInfo(GetContentPath());
+            DirectoryInfo dir = new DirectoryInfo(folder);
 
             if (dir.Exists)
             {
@@ -139,6 +144,11 @@ public class WorkshopEditorWindow : EditorWindow
             {
                 EditorGUILayout.HelpBox("Change notes must be entered before publishing to Workshop", MessageType.Warning);
             }
+            
+            if(dir.Exists && dir.GetFiles("modInfo_Harmony.json").Length > 0)
+			{
+				EditorGUILayout.HelpBox("Your mod uses the Harmony library. This means it won't work without the Tweaks mod, so on the Workshop, please either add Tweaks as a dependency or mention it in the description!", MessageType.Warning);
+			}
 
 
             //Publishing changes
@@ -162,7 +172,7 @@ public class WorkshopEditorWindow : EditorWindow
                 {
                     PublishWorkshopChanges();
                 }
-
+				
                 if (!string.IsNullOrEmpty(ugcUpdateStatus))
                 {
                     EditorGUILayout.LabelField(ugcUpdateStatus);
@@ -170,6 +180,7 @@ public class WorkshopEditorWindow : EditorWindow
 
                 GUI.enabled = true;
             }
+            
             EditorGUILayout.EndVertical();
             EditorGUILayout.EndScrollView();
         }
